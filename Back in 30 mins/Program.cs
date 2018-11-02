640883a63ab73f6ed4bc2469be66c001f7b3c243
Nekster <nekster187@gmail.com>
2018-11-02 19:06:11 +0200

    Create Program.cs
    Every time Stamat tries to pay his bills he sees on the cash desk the sign: "I will be back in 30 minutes". One day Stamat was sick of waiting and decided he needs a program, which prints the time after 30 minutes. That way he won’t have to wait on the desk and come at the appropriate time. He gave the assignment to you, so you have to do it.

---
+++ Back in 30 mins/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back_in_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 30;
+            while (minutes >= 60)
+            {
+                minutes -= 60;
+                hours++;
+            }
+
+            while (hours >= 24)
+            {
+                hours -= 24;
+            }
+
+            Console.WriteLine($"{hours}:{minutes:D2}");
+
+        }
+    }
+}
