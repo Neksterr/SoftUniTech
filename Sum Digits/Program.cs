ed155f7bc545102b2e147852833a0c369f11767d
Nekster <nekster187@gmail.com>
2018-11-02 23:44:41 +0200

    Create Program.cs

---
+++ Sum Digits/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            while (number != 0)
+            {
+                sum += number % 10;
+                number = number / 10;
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
