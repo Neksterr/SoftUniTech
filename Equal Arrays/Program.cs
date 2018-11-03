39197bf4377aae9572cec22569819000764a62d2
Nekster <nekster187@gmail.com>
2018-11-03 22:08:42 +0200

    Create Program.cs

---
+++ Equal Arrays/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equal_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] firstArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] secondArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int sum = 0;
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                       if (firstArray[i] != secondArray[i])
+                       {
+                         Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                          return;
+                       }
+                
+                        
+                         
+            }
+                Console.WriteLine($"Arrays are identical. Sum: {firstArray.Sum()}");
+                
+        }
+    }
+}
